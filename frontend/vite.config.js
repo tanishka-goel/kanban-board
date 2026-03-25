e827e4eae30cd7f084e2c6d196d0fdd2b6479d91
tanishkag237 <tgoel029@gmail.com>
2026-03-25 11:35:16 +0530

    fs revamp

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 import path from "path"
-import { fileURLToPath } from "url"
 
 // https://vite.dev/config/
 export default defineConfig({
+  envDir: "..",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
