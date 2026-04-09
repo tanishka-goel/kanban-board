b29467160253872ad4f925dc85092a0a05778029
tanishkag237 <tgoel029@gmail.com>
2026-04-09 17:40:50 +0530

    fixed port

--- backend/index.js
+++ backend/index.js
@@ -121,6 +121,6 @@ app.get("/api/messages", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
