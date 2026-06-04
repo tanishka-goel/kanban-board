c8250cf2cc7fc17034380d5c7a26fb64d589b600
tanishkag237 <tgoel029@gmail.com>
2026-06-04 11:34:23 +0530

    fixed errors 2

--- frontend/src/api/messages.api.js
+++ frontend/src/api/messages.api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+const ChatApi = axios.create({
+  baseURL:import.meta.env.VITE_CHAT_SERVER_URL || "http://localhost:3000",
+  timeout:10000
+})
 
-const CHAT_API_URL = import.meta.env.VITE_CHAT_SERVER_URL || "http://localhost:3000";
 
 export async function getMessages( sender_id, receiver_id ) {
-  const { data } = await axios.get(`${CHAT_API_URL}/api/messages`, {
+  const { data } = await ChatApi.get("/api/messages", {
     params: { sender_id, receiver_id },
   });
   return data;
--- frontend/src/api/users.api.js
+++ frontend/src/api/users.api.js
@@ -6,22 +6,22 @@ export async function getUsers(){
     return response.data
 }
 
-export async function createUsers(newData){
-   try{
-     const response = await BaseApi.post(`/rest/v1/profiles`,newData);
-    return response.data 
-   } catch (error){
-    const supabaseError = error?.response?.data
+// export async function createUsers(newData){
+//    try{
+//      const response = await BaseApi.post(`/rest/v1/profiles`,newData);
+//     return response.data 
+//    } catch (error){
+//     const supabaseError = error?.response?.data
 
-    if (supabaseError?.code === "23505") {
-        const details = supabaseError?.details || ""
-        if (details.toLowerCase().includes("username")) {
-        throw { response: { data: { error: "Username already exists." } } };
-      }
-    }
-    throw error
-   }
-}
+//     if (supabaseError?.code === "23505") {
+//         const details = supabaseError?.details || ""
+//         if (details.toLowerCase().includes("username")) {
+//         throw { response: { data: { error: "Username already exists." } } };
+//       }
+//     }
+//     throw error
+//    }
+// }
 
 export async function updateUsers({id,newData}){
     const response = await BaseApi.patch(`/rest/v1/profiles?id=eq.${id}`, newData);
